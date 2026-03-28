2776dc3a9168512068994495cef0f35091232a68
Doğukan İyidoğan <69318878+dogukaniyidogan@users.noreply.github.com>
2026-03-28 22:19:29 +0300

    Site v1

--- src/js/index.js
+++ src/js/index.js
@@ -1,9 +1,9 @@
 const glow = document.querySelector(".cursor-glow");
 
-window.addEventListener("mousemove", (e) => {
-  glow.style.left = e.clientX + "px";
-  glow.style.top = e.clientY + "px";
-});
+// window.addEventListener("mousemove", (e) => {
+//   glow.style.left = e.clientX + "px";
+//   glow.style.top = e.clientY + "px";
+// });
 
 
 const sections = document.querySelectorAll("section[id]");
@@ -30,10 +30,44 @@ const observer = new IntersectionObserver(
   },
   {
     root: null,
-    threshold: 0.6 // section'ın %60'ı görünür olunca aktif
+    threshold: 0.1 // section'ın %60'ı görünür olunca aktif
   }
 );
 
 sections.forEach((section) => {
   observer.observe(section);
 });
+
+
+
+// başlangıçta temayı yükle
+function loadTheme() {
+  const saved = localStorage.getItem("theme");
+
+  if (saved) {
+    document.documentElement.setAttribute("data-theme", saved);
+  } else {
+    // sistem temasını algıla
+    const prefersDark = window.matchMedia("(prefers-color-scheme: dark)").matches;
+    document.documentElement.setAttribute(
+      "data-theme",
+      prefersDark ? "dark" : "light"
+    );
+  }
+}
+
+// toggle
+function toggleTheme() {
+  const current = document.documentElement.getAttribute("data-theme");
+
+  const newTheme = current === "dark" ? "light" : "dark";
+
+  document.documentElement.setAttribute("data-theme", newTheme);
+  localStorage.setItem("theme", newTheme);
+}
+
+// event
+document.querySelector(".alt .toggle-color-scheme").addEventListener("click", toggleTheme);
+
+// init
+loadTheme();
